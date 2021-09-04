943a1ea0b6162da6a0010599bffe85fad207b61f
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-04 23:21:04 +0300

    Create day-02.js

---
+++ day-02.js
@@ -0,0 +1,28 @@
+//https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+  var a=v1;  
+  var b=v1;   
+  return a+b;
+}
+function equal2(){
+  var a=v3; 
+  var b=v1;   
+  return a-b;
+}
+function equal3(){
+  var a=v1;   
+  var b=v5;   
+  return a*b;
+}
+function equal4(){
+  var a=v4;  
+  var b=v5;   
+  return a/b;
+}
+function equal5(){
+  var a=v6;  
+  var b=v3;   
+  return a%b;
+}
+//
