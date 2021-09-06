ba68ea40e4fbaa34f2b2919b370794d065d864d3
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-06 16:04:25 +0300

    Create day-09.js

---
+++ day-09.js
@@ -0,0 +1,8 @@
+//http://www.codewars.com/kata/57274562c8dcebe77e001012
+
+//http://www.codewars.com/kata/57277a31e5e51450a4000010
+function firstToLast(str, c) {
+  return str.indexOf(c) === -1 ? -1 : str.lastIndexOf(c) - str.search(c);
+}
+
+//http://www.codewars.com/kata/57280481e8118511f7000ffa
