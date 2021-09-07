36b29fe404b4de0dde7eacd22eda06e5e2588f0e
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-07 14:15:41 +0300

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -1,4 +1,13 @@
 //http://www.codewars.com/kata/57274562c8dcebe77e001012
+function cutIt(arr) {
+  const newArr = [];
+  const minStr = arr.reduce((a, b) => (a.length < b.length ? a : b));
+  for (let i = 0; i < arr.length; i++) {
+    let newStr = arr[i].substr(0, minStr.length);
+    newArr.push(newStr);
+  }
+  return newArr;
+}
 
 //http://www.codewars.com/kata/57277a31e5e51450a4000010
 function firstToLast(str, c) {
@@ -6,3 +15,7 @@ function firstToLast(str, c) {
 }
 
 //http://www.codewars.com/kata/57280481e8118511f7000ffa
+function splitAndMerge(string, separator) {
+  const newStr = string.split(' ');
+  return newStr.map((str) => str.split('').join(separator)).join(' ');
+}
