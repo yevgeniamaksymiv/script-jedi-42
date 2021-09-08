0d2e426e846f069a9d3e4158c0d0db5d95beb229
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-08 17:23:49 +0300

    Update day-12.js

--- day-12.js
+++ day-12.js
@@ -8,3 +8,15 @@ function wordToBin(str) {
 }
 
 //https://www.codewars.com/kata/57f75cc397d62fc93d000059
+function calc(x) {
+  const total1 = x
+    .split('')
+    .map((char) => char.charCodeAt())
+    .join('');
+  const total2 = total1.replace(/['7']/g, '1');
+  const digitSum1 = total1.split('');
+  const sum1 = digitSum1.reduce((a, b) => Number(a) + Number(b));
+  const digitSum2 = total2.split('');
+  const sum2 = digitSum2.reduce((c, d) => Number(c) + Number(d));
+  return sum1 - sum2;
+}
