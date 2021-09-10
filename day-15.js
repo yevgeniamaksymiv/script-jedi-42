c57485baa958318196cc7e1ef94d280dc3d427a7
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-10 15:36:27 +0300

    Create day-15.js

---
+++ day-15.js
@@ -0,0 +1,35 @@
+//http://www.codewars.com/kata/572ab0cfa3af384df7000ff8
+function shuffleIt(arr, ...xArr) {
+  for (let i = 0; i < xArr.length; i++) {
+    let [a, b] = [xArr[i][0], xArr[i][1]];
+    [arr[b], arr[a]] = [arr[a], arr[b]];
+  }
+  return arr;
+}
+
+//https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50/
+function loopArr(arr, direction, steps) {
+  if (direction === 'left') {
+    let arr1 = arr.splice(0, steps);
+    return [...arr, ...arr1];
+  } else {
+    let arr2 = arr.splice(-steps);
+    return [...arr2, ...arr];
+  }
+}
+
+//http://www.codewars.com/kata/572af273a3af3836660014a1
+function infiniteLoop(arr, d, n) {
+  for (let i = 1; i <= n; i++) {
+    if (d === 'left') {
+      arr[2].push(arr[0].shift());
+      arr[1].push(arr[2].shift());
+      arr[0].push(arr[1].shift());
+    } else {
+      arr[0].unshift(arr[2].pop());
+      arr[1].unshift(arr[0].pop());
+      arr[2].unshift(arr[1].pop());
+    }
+  }
+  return arr;
+}
