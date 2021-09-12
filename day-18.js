f6b1ec9b844cc3f4085bac8d16264ffff7cf60ba
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-12 21:37:51 +0300

    Create day-18.js

---
+++ day-18.js
@@ -0,0 +1,12 @@
+//http://www.codewars.com/kata/573156709a231dcec9000ee8
+function tailAndHead(arr) {
+  const newArr = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    const x = arr[i].toString().split('').slice(-1);
+    const y = arr[i + 1].toString().split('').slice(0, 1);
+    newArr.push(parseInt(x) + parseInt(y));
+  }
+  return newArr.reduce((a, b) => a * b);
+}
+
+//http://www.codewars.com/kata/5732b0351eb838d03300101d
