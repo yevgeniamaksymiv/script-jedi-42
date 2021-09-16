123bd6e4507f5fc4d9c277142d1fd00d206eb888
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-16 20:25:18 +0300

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -2,5 +2,5 @@
 var regex = /^[-?9].*0{4}$/;
 
 //http://www.codewars.com/kata/573d11c48b97c0ad970002d4
-var regex = /https?:\/\/\w{3}?\.?[a-z0-9.]{0,}(\.com|\.net){1,}/gi;
+var regex = /https?:\/\/[a-z0-9.]{1,}(\.com|\.net)/gi;
 
