19e258e0bf08570e06b9257e363d9260d6459b38
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-22 14:42:53 +0300

    Create day-13.js

---
+++ day-13.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/56c22c5ae8b139416c00175d/
+function match(candidate, job) {
+  if (candidate.minSalary * 0.9 <= job.maxSalary) {
+    return true;
+  } else if (candidate.minSalary * 0.9 >= job.maxSalary) {
+    return false;
+  } else throw new Error();
+}
+
+//https://www.codewars.com/kata/56c2578be8b139bd5c001bd8
