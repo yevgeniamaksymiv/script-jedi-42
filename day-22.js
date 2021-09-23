812700dcee4101f3b9ab8ba07e6f977013e35cd2
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-23 11:41:54 +0300

    Create day-22.js

---
+++ day-22.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/559f860f8c0d6c7784000119/train/javascript
+
+
+//https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
+const multiplyAll = (arr) => (n) => arr.map((el) => el * n);
