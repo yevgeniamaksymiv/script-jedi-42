d69ef8831ece9d88b116d7c62894e6694ea1ff35
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-24 19:21:07 +0300

    Create day-26.js

---
+++ day-26.js
@@ -0,0 +1,7 @@
+//http://www.codewars.com/kata/power-bind
+
+
+//https://www.codewars.com/kata/sort-with-arrow-functions
+var OrderPeople = function (people) {
+  return people.sort((a, b) => a.age - b.age);
+};
