fc6d4ab2d9e81976dfa88049c4a0cdceedc7b06a
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-25 14:34:15 +0300

    Update day-22.js

--- day-22.js
+++ day-22.js
@@ -1,5 +1,7 @@
 //https://www.codewars.com/kata/559f860f8c0d6c7784000119/train/javascript
-
+function anyArrows(arrows) {
+  return arrows.some((ar) => !ar.damaged);
+}
 
 //https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
 const multiplyAll = (arr) => (n) => arr.map((el) => el * n);
