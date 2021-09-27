7298315f8c20c0d3b844aa175fb1aa8b6bb7e11f
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-27 17:09:58 +0300

    Update day-26.js

--- day-26.js
+++ day-26.js
@@ -1,5 +1,11 @@
 //http://www.codewars.com/kata/power-bind
-
+Function.prototype.bind = function (ctx) {
+  const func = this;
+  return function (...args) {
+    const rightCtx = this === global ? ctx : this;
+    return func.apply(rightCtx, args);
+  };
+};
 
 //https://www.codewars.com/kata/sort-with-arrow-functions
 var OrderPeople = function (people) {
