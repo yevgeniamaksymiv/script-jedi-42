eabe197ade3f39f16d6ff16712cebe66f6a57f82
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-27 10:35:12 +0300

    Update day-12.js

--- day-12.js
+++ day-12.js
@@ -1,10 +1,10 @@
 //https://www.codewars.com/kata/59859f435f5d18ede7000050
 function wordToBin(str) {
-  const array = str
-    .split('')
-    .map((char) => '0' + char.charCodeAt(0).toString(2).slice(-8))
-    .join(' ');
-  return array.split(' ');
+  return str
+    .split("")
+    .map((char) => "0" + char.charCodeAt(0).toString(2).slice(-8))
+    .join(" ")
+    .split(" ");
 }
 
 //https://www.codewars.com/kata/57f75cc397d62fc93d000059
