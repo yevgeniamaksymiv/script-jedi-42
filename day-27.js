13f0ae8c1c393b939d52240cea83a352229cac87
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-29 22:25:01 +0300

    Create day-27.js

---
+++ day-27.js
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
+function buildFun(n) {
+  let res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i;
+    });
+  }
+  return res;
+}
+
+//https://www.codewars.com/kata/shifty-closures/train/javascript
+const greet_abe = function () {
+  const name = "Abe";
+  return "Hello, " + name + "!";
+};
+const greet_ben = function () {
+  const name = "Ben";
+  return "Hello, " + name + "!";
+};
