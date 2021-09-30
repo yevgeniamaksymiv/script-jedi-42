9c5da7a66ac139af591f77a15e7074aa1246150a
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-09-30 12:16:46 +0300

    Update day-28.js

--- day-28.js
+++ day-28.js
@@ -6,10 +6,11 @@ function always(n) {
 }
 
 //https://www.codewars.com/kata/59bdbe9d46038724ca0000b9/train/javascript
+const state = {
+  modifier: 2,
+};
 function solution(arr, options) {
-  const state = {
-    modifier: 2,
-  };
   const newArr = arr.map((el) => el + 2 * options.modifier);
   return newArr;
 }
+
