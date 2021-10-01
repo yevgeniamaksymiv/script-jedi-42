1b683ef64fcc45446fc04b355c3cb8ebafc9b62a
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-01 18:37:22 +0300

    Update day-24.js

--- day-24.js
+++ day-24.js
@@ -5,3 +5,11 @@ function solution() {
 }
 
 //http://www.codewars.com/kata/last
+function last(list) {
+  const arg = [...arguments];
+  if (Array.isArray(list) === true) return list.pop();
+  if (typeof list === "string")
+    return arg[arg.length - 1][arg[arg.length - 1].length - 1];
+  if (typeof list !== "string" || Array.isArray(list) === false)
+    return arg[arg.length - 1];
+}
