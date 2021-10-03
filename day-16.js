b555b7bc3136c1702215d0d6274b8becdcfd3dca
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-03 12:40:41 +0300

    Update day-16.js

--- day-16.js
+++ day-16.js
@@ -1,6 +1,6 @@
 //http://www.codewars.com/kata/572cb264362806af46000793
 function threeInOne(arr) {
-  var sumArr = arr.slice();
+  const sumArr = arr.slice();
   for (let i = arr.length - 1; i >= 0; i -= 3) {
     let sum = arr[i] + arr[i - 1] + arr[i - 2];
     sumArr.splice(i, 3, sum);
@@ -24,7 +24,7 @@ function sortIt(arr) {
 
 //http://www.codewars.com/kata/572fdeb4380bb703fc00002c
 function isolateIt(arr) {
-  let newArr = arr.map((x, i) => {
+  const newArr = arr.map((x, i) => {
     if (arr[i].length % 2 === 0) {
       return (
         x.slice(0, Math.floor(arr[i].length / 2)) +
