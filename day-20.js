add9ccb1cddd168c722df50e85672441d226cfc0
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-03 13:22:56 +0300

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -1,6 +1,6 @@
 //https://www.codewars.com/kata/573bca07dffc1aa693000139
-var regex = /^[-?9].*0{4}$/;
+const regex = /^[-?9].*0{4}$/;
 
 //http://www.codewars.com/kata/573d11c48b97c0ad970002d4
-var regex = /https?:\/\/[a-z0-9.]{1,}(\.com|\.net)/gi;
+const regex = /https?:\/\/[a-z0-9.]{1,}(\.com|\.net)/gi;
 
