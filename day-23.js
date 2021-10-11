90d3660c22d05f47f9d4be3f7df0f52a8b552cb5
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-11 15:57:50 +0300

    Update day-23.js

--- day-23.js
+++ day-23.js
@@ -1,7 +1,5 @@
 //http://www.codewars.com/kata/unpacking-arguments
-function spread(func, args) {
-  return func.apply(this, args);
-}
+const spread = (func, args) => func.apply(this, args);
 
 //http://www.codewars.com/kata/for-the-sake-of-argument
 const numbers = (...num) => num.every((el) => typeof el === "number");
