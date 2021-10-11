685e48a2985d4b11ccd2df0c1d39e7841d5be11e
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-11 17:47:57 +0300

    Create day-34.js

---
+++ day-34.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/basic-subclasses-adam-and-eve/
+
+//https://www.codewars.com/kata/56ff9b53140fcca90b000530/
+class Labrador extends Dog {
+  constructor(name, age, gender, master) {
+    super(name, age, gender, "Labrador", "Large", master, true);
+  }
+}
+
+//https://www.codewars.com/kata/55a144eff5124e546400005a
