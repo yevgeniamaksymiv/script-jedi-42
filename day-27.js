69e8639174487afc7d98baf756b40ae9f199ebe4
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-11 16:19:18 +0300

    Update day-27.js

--- day-27.js
+++ day-27.js
@@ -1,6 +1,6 @@
 //https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
 function buildFun(n) {
-  let res = [];
+  const res = [];
   for (let i = 0; i < n; i++) {
     res.push(function () {
       return i;
