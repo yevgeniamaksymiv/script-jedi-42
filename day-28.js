b6ea36eed70bff8843bd52d4ae4bafccdc11e252
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-11 16:27:05 +0300

    Update day-28.js

--- day-28.js
+++ day-28.js
@@ -1,16 +1,12 @@
 //http://www.codewars.com/kata/a-function-within-a-function
-function always(n) {
-  return function () {
-    return n;
-  };
-}
+const always = (n) => () => n;
 
 //https://www.codewars.com/kata/59bdbe9d46038724ca0000b9/train/javascript
 const state = {
   modifier: 2,
 };
 function solution(arr, options) {
-  const newArr = arr.map((el) => el + 2 * options.modifier);
-  return newArr;
+  const modifierArr = arr.map((el) => el + 2 * options.modifier);
+  return modifierArr;
 }
 
