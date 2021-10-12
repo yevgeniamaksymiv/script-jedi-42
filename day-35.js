b82f7a7059a15750a9e0f5adbb956b3351f7db47
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-12 15:49:25 +0300

    Create day-35.js

---
+++ day-35.js
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
+Object.prototype.description = "TODO: GIVE ME A DESCRIPTION";
+
+//https://www.codewars.com/kata/javascript-class-like-objects
