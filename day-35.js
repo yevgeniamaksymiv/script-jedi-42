6e6ceb199f8ed2f24ea72c9cd11922bcb60cde37
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-12 16:21:59 +0300

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -2,3 +2,15 @@
 Object.prototype.description = "TODO: GIVE ME A DESCRIPTION";
 
 //https://www.codewars.com/kata/javascript-class-like-objects
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  get animalInfo() {
+    return this.toString();
+  }
+  toString() {
+    return this.name + " is a " + this.type;
+  }
+}
