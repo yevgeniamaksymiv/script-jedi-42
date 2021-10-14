b7d38ca92e072f829c97d7a61a3175a38293a3a3
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-14 13:56:50 +0300

    Update day-30.js

--- day-30.js
+++ day-30.js
@@ -8,3 +8,19 @@ function isSantaClausable(obj) {
 }
 
 //http://www.codewars.com/kata/cylon-evolution
+class Cylon {
+  constructor(model) {
+    this.model = model;
+  }
+  attack() {
+    return "Destroy all humans!";
+  }
+}
+class HumanSkin extends Cylon {
+  constructor(model) {
+    super(model);
+  }
+  infiltrate() {
+    return "Infiltrate the colonies";
+  }
+}
