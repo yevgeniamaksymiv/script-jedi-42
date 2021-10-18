07f834bfa281ceaca5355e76a3fd6f9d548fba38
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-18 15:55:34 +0300

    Update day-29.js

--- day-29.js
+++ day-29.js
@@ -3,3 +3,13 @@ const myObj = Object.create(MyObject.prototype);
 MyObject.call(myObj);
 
 //http://www.codewars.com/kata/replicate-new
+function nouveau(Constructor, ...args) {
+  const person = Object.create(Constructor.prototype);
+  const constructorResult = Constructor.apply(person, args);
+  if (
+    typeof constructorResult === "function" ||
+    (typeof constructorResult === "object" && constructorResult !== null)
+  )
+    return constructorResult;
+  return person;
+}
