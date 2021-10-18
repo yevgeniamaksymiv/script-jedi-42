033c3fb1033d6123674ca1f2273ae219c5b375fa
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-18 13:44:38 +0300

    Update day-32.js

--- day-32.js
+++ day-32.js
@@ -7,3 +7,16 @@ function NameMe(first, last) {
 }
 
 //https://www.codewars.com/kata/this-is-an-other-problem
+class NamedOne {
+  constructor(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  }
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  set fullName(value) {
+    const regex = /^([\w]{3,})+\s+([\w\s]{3,})+$/i;
+    if (regex.test(value)) [this.firstName, this.lastName] = value.split(" ");
+  }
+}
