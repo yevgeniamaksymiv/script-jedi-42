f9cf7e297e3f5facac01bc6892b09f82705010c0
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-26 13:28:49 +0300

    Create day-42.js

---
+++ day-42.js
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/two-sum/
+function twoSum(nums, target) {
+  let resultArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) resultArr.push(i, j);
+    }
+  }
+  return resultArr;
+}
