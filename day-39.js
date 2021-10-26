5a1ece72f2797d5a169f0847a48a36aa898df11e
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-26 15:34:50 +0300

    Update day-39.js

--- day-39.js
+++ day-39.js
@@ -1,6 +1,9 @@
 //https://www.codewars.com/kata/5637ead70013386e30000027/
 function* generator(a) {
-  for (let i = 1; i < 100; i++) {
+  let i = 1;
+  while (true) {
     yield `${a} x ${i} = ${a * i}`;
+    i++;
   }
 }
+
