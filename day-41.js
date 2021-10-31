f2a2e52c051783512047d43bd5e005e9f5e640d4
yevgeniamaksymiv <88046672+yevgeniamaksymiv@users.noreply.github.com>
2021-10-31 12:26:03 +0200

    Update day-41.js

--- day-41.js
+++ day-41.js
@@ -1,5 +1,12 @@
 //https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/javascript
-
+function queueTime(customers, n) {
+  const arrTills = Array(n).fill(0);
+  for (const i in customers) {
+    let freeTill = arrTills.indexOf(Math.min(...arrTills));
+    arrTills[freeTill] += customers[i];
+  }
+  return Math.max(...arrTills);
+}
 
 //https://www.codewars.com/kata/567ed5db4089538eea000010/train/javascript
 function launchAll(launchMissile) {
